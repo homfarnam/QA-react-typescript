27fc6c3f68d39c73feeb11f81b3f3b8604f556e8
Farnam Homayounfar <homfarnam@gmail.com>
2022-07-29 19:00:57 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-screen text-white bg-black">
+      <div className="container flex justify-center mx-auto w-full">
+        <h1>React - Typescript - Tailwind - Prettier - eslint</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
