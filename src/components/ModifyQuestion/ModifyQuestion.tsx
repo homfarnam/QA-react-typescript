6d7b2ce6858a452176402c72b98bc89f3f9126a6
Farnam Homayounfard <farnam@getopensocial.com>
2022-07-31 00:50:57 +0200

    Create handle edit questions
    - Create edit button
    - Send selected question for edit to reducer

--- src/components/ModifyQuestion/ModifyQuestion.tsx
+++ src/components/ModifyQuestion/ModifyQuestion.tsx
@@ -1,22 +1,49 @@
 import Button from "components/Button/Button"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
-import { addQuestion } from "store/features/questionsSlice"
+import {
+  addQuestion,
+  removeSelectedQuestion,
+  editQuestion,
+} from "store/features/questionsSlice"
+import type { ModifyQuestionProps, QuestionType } from "types/types"
 import { v4 as uuidv4 } from "uuid"
 
-const ModifyQuestion: React.FC = () => {
-  const [question, setQuestion] = useState("")
-  const [answer, setAnswer] = useState("")
+const initialState = {
+  id: "",
+  question: "",
+  answer: "",
+}
+
+const ModifyQuestion: React.FC<ModifyQuestionProps> = ({
+  selectedQuestion,
+}) => {
+  const [qaData, setQaData] = useState<QuestionType>(initialState)
+
   const [delay, setDelay] = useState(false)
   const [loading, setLoading] = useState<boolean>(false)
 
   const dispatch = useDispatch()
 
+  useEffect(() => {
+    if (selectedQuestion) {
+      setQaData(selectedQuestion)
+    }
+  }, [selectedQuestion])
+
+  const handleEdit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    dispatch(editQuestion(qaData))
+
+    resetForm()
+  }
+
   const submit = () => {
     dispatch(
       addQuestion({
-        answer,
-        question,
+        answer: qaData.answer,
+        question: qaData.question,
         id: uuidv4(),
       })
     )
@@ -24,10 +51,8 @@ const ModifyQuestion: React.FC = () => {
     resetForm()
   }
 
-  const handleSubmitForm = (
-    _e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
-  ) => {
-    if (!question || !answer) {
+  const handleSubmitForm = () => {
+    if (!qaData.question || !qaData.answer) {
       return
     }
 
@@ -37,24 +62,19 @@ const ModifyQuestion: React.FC = () => {
         submit()
       }, 5000)
     } else {
-      // setLoading(true)
       submit()
     }
   }
 
   const resetForm = () => {
-    setQuestion("")
-    setAnswer("")
+    setQaData(initialState)
     setDelay(false)
   }
   return (
-    <form
-      className="flex flex-col justify-start items-start px-3 w-full"
-      onSubmit={(e) => handleSubmitForm(e)}
-    >
-      <h3 className="text-xl font-bold">Create a new question</h3>
+    <form className="mainform" onSubmit={(_e) => handleSubmitForm()}>
+      <h3 className="mainform--title">Create a new question</h3>
 
-      <div className="flex flex-col mt-5 w-full">
+      <div className="mainform--qa">
         <label htmlFor="question">Question</label>
         <input
           type="text"
@@ -62,26 +82,28 @@ const ModifyQuestion: React.FC = () => {
           autoFocus
           required
           placeholder="write a new quesiton"
-          className="py-3 px-3 mt-3 w-3/4 rounded-xl border outline-none"
-          onChange={(e) => setQuestion(e.target.value)}
-          value={question}
+          onChange={(e) => {
+            setQaData({ ...qaData, question: e.target.value })
+          }}
+          value={qaData.question}
         />
       </div>
 
-      <div className="flex flex-col mt-5 w-full">
+      <div className="mainform--qa">
         <label htmlFor="answer">Answer</label>
         <input
           type="text"
           required
           name="answer"
           placeholder="write the answer"
-          className="py-3 px-3 mt-3 w-3/4 rounded-xl border outline-none"
-          onChange={(e) => setAnswer(e.target.value)}
-          value={answer}
+          onChange={(e) => {
+            setQaData({ ...qaData, answer: e.target.value })
+          }}
+          value={qaData.answer}
         />
       </div>
 
-      <div className="flex gap-3 justify-start items-center mt-5 w-full">
+      <div className="mainform--check">
         <label htmlFor="delay">Delay</label>
         <input
           type="checkbox"
@@ -91,12 +113,21 @@ const ModifyQuestion: React.FC = () => {
         />
       </div>
 
-      <Button
-        className="my-5 bg-green-400 rounded-lg"
-        text={loading ? "Loading..." : "Add question"}
-        onClick={(e) => handleSubmitForm(e)}
-        disabled={loading}
-      />
+      <div className="mainform--buttons">
+        <Button
+          className="mainform--buttons__add"
+          text={loading ? "Loading..." : "Add question"}
+          onClick={(_e) => handleSubmitForm()}
+          disabled={loading || selectedQuestion ? true : false}
+        />
+
+        <Button
+          className="mainform--buttons__edit"
+          text="Edit question"
+          onClick={(e) => handleEdit(e)}
+          disabled={!selectedQuestion}
+        />
+      </div>
     </form>
   )
 }
