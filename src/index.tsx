25bdec3d512c7e325390c33024bb4ef1e9de93c2
Farnam Homayounfard <farnam@getopensocial.com>
2022-07-30 22:57:33 +0200

    Add index
    - Using redux provider to share states between components
    
    - Update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import App from "./App"
 import "./styles/tailwind.css"
 import "./styles/index.css"
+import { Provider } from "react-redux"
+import { store } from "store/store"
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
-  <React.StrictMode>
+const container = document.getElementById("root")
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>
 )
