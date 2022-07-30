ece3643928f7a11883e147dcf19f9e91949059b9
Farnam Homayounfard <farnam@getopensocial.com>
2022-07-31 00:40:32 +0200

    Remove unused props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,14 +5,12 @@ interface ButtonProps
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   > {
-  variant?: "black" | "white" | string
   noPaddings?: boolean
   fontSize?: string
   text: string
 }
 
 const Button: React.FC<ButtonProps> = ({
-  variant = "",
   className = "",
   fontSize,
   noPaddings = false,
@@ -24,15 +22,9 @@ const Button: React.FC<ButtonProps> = ({
     <button
       disabled={disabled}
       className={`rounded-full font-bold sm:leading-9
-       ${fontSize ? fontSize : "text-lg"} ${noPaddings ? "" : "py-3 px-8"} ${
-        variant === "black"
-          ? "bg-black"
-          : variant === "white"
-          ? "bg-white"
-          : null
-      } ${variant === "black" ? "text-white" : "text-black"} ${
-        disabled ? "opacity-50" : ""
-      } ${className}`}
+       ${fontSize ? fontSize : "md:text-lg"} ${
+        noPaddings ? "" : "py-3 md:px-8 px-3"
+      } ${disabled ? "opacity-50" : ""} ${className}`}
       {...rest}
     >
       {text}
