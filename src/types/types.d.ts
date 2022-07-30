0069b14a5d361bc4328d4ba1765e769e84aa6625
Farnam Homayounfard <farnam@getopensocial.com>
2022-07-31 00:39:12 +0200

    Create types file
    - Add all interfaces and export them

---
+++ src/types/types.d.ts
@@ -0,0 +1,31 @@
+interface QuestionsProps {
+  data: QuestionType
+  onEdit: (data: QuestionType) => void
+}
+
+interface QuestionsListProps {
+  data: QuestionType[]
+}
+
+interface QuestionType {
+  id: string
+  question: string
+  answer: string
+}
+
+interface State {
+  questions: QuestionType[]
+  selectedQuestion?: QuestionType
+}
+
+interface ModifyQuestionProps {
+  selectedQuestion: QuestionType
+}
+
+export {
+  QuestionsProps,
+  QuestionsListProps,
+  QuestionType,
+  State,
+  ModifyQuestionProps,
+}
