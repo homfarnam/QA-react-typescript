389f4e3b93f49ab134b425fa302bd9c019e1cb21
Farnam Homayounfard <farnam@getopensocial.com>
2022-07-31 00:39:24 +0200

    Import main scss file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import { Provider } from "react-redux"
+import { createRoot } from "react-dom/client"
 import App from "./App"
+import { store } from "store/store"
 import "./styles/tailwind.css"
 import "./styles/index.css"
-import { Provider } from "react-redux"
-import { store } from "store/store"
-import { createRoot } from "react-dom/client"
+import "./styles/main.scss"
 
 const container = document.getElementById("root")
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
